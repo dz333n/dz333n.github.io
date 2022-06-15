cd291650d2d58fb10d2239de5464f7efd3b55b89
Yaroslav Kibysh <yanet.prod@gmail.com>
2022-06-15 21:18:52 +0300

    Add WTC build

---
+++ wtc/precache-manifest.408a043a9ff627728696cab5e21e0ec7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7d311f88701175b46783082a7f6106",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ade913e743c12667559",
+    "url": "/static/css/main.329b1865.chunk.css"
+  },
+  {
+    "revision": "ca0e9d0fd78a7830177f",
+    "url": "/static/js/2.8311be9e.chunk.js"
+  },
+  {
+    "revision": "2187e117ce5a4ac7c4e0eb90961457eb",
+    "url": "/static/js/2.8311be9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ade913e743c12667559",
+    "url": "/static/js/main.0b5b5c65.chunk.js"
+  },
+  {
+    "revision": "3cc625bd412b7f4f83f0",
+    "url": "/static/js/runtime-main.d2b01076.js"
+  }
+]);
